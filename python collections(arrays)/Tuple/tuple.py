70200aa76db035da339c4ef7932bf38689ab7b58
Priyanka <129821118+Gounderpriyanka@users.noreply.github.com>
2025-07-22 15:27:49 +0530

    Update tuple.py

--- python collections(arrays)/Tuple/tuple.py
+++ python collections(arrays)/Tuple/tuple.py
@@ -10,4 +10,22 @@ color = ("red","yellow","blue","pink")
 
 print(color[1])#positive indexes
 print(color[-1])#negative indexes
-print(color[1:3])
+print(color[1:3]) #range indexing
+print(color[-2:]) #negative range indexing
+
+#cheking an item in tuple
+if "green" in color:
+    print("yes")
+
+#print the tuples
+for i in color:
+    print(i)
+
+#concatenate the tuple
+# new_color = ("green","orange")
+# color += new_color 
+# print(color) 
+
+#unpacking
+color1, color2, color3, color4 = color
+print(color1, color2, color3, color4)
