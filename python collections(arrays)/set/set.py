c26c140afb3493fc61626dc900d7e70206a8a0c9
Gounderpriyanka <gounderpriyanka4@gmail.com>
2025-12-01 18:06:46 +0530

    Updated project files and added new Python scripts

--- python collections(arrays)/set/set.py
+++ python collections(arrays)/set/set.py
@@ -1,23 +1,40 @@
-num = {"asd", 3 , "eds"}
-print(num)
-print(type(num))
-print(len(num))
+# num = {"asd", 3 , "eds"}
+# print(num)
+# print(type(num))
+# print(len(num))
 
-for i in num:
-    print(i)
+# for i in num:
+#     print(i)
 
-if "sia" in num:
-    print("yes")
+# if "sia" in num:
+#     print("yes")
 
-num.add("mik")
-print(num)
+# num.add("mik")
+# print(num)
 
-names = { "aed", "lila", 34}
-num.update(names)
-print(num)
+# names = { "aed", "lila", 34}
+# num.update(names)
+# print(num)
 
-num.remove("lila")
-print(num)
+# num.remove("lila")
+# print(num)
 
-num.discard("lion")
-print(num)
+# num.discard("lion")
+# print(num)
+
+
+s1 = {1, 2, 3}
+s2 = {4, 5, 6,2,3}
+
+# s3 = s1.union(s2)
+# print(s3)
+
+# s1.update(s2)
+# print(s1)
+
+# s1.intersection_update(s2)
+# print(s1)
+
+s1.symmetric_difference_update(s2)
+print(s1)
+
