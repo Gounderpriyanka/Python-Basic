f7dfc27e4786fbf9c9b434ddeb0cb5275c917da5
Gounderpriyanka <gounderpriyanka4@gmail.com>
2025-12-01 14:11:20 +0530

    Updated project files and added new Python scripts

--- python collections(arrays)/set/set.py
+++ python collections(arrays)/set/set.py
@@ -5,3 +5,19 @@ print(len(num))
 
 for i in num:
     print(i)
+
+if "sia" in num:
+    print("yes")
+
+num.add("mik")
+print(num)
+
+names = { "aed", "lila", 34}
+num.update(names)
+print(num)
+
+num.remove("lila")
+print(num)
+
+num.discard("lion")
+print(num)
