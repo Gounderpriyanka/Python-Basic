abfaa23aa273a7549f938ec65605c357894ad0b4
Gounderpriyanka <gounderpriyanka4@gmail.com>
2025-12-01 21:48:01 +0530

    updated project files and add new python scripts

--- python collections(arrays)/set/example1.py
+++ python collections(arrays)/set/example1.py
@@ -1,4 +1,18 @@
-ar1 = [1,5,10,20,40,80]
-ar2 = [6,7,20,80,100]
-ar3 = [3,4,15,20,30,70,80,120]
+# ar1 = [1,5,10,20,40,80]
+# ar2 = [6,7,20,80,100]
+# ar3 = [3,4,15,20,30,70,80,120]
+
+ar1 = [1,5,5]
+ar2 = [3,4,5,5,10]
+ar3 = [5,5,10,20,1]
+
+s1 = set(ar1)
+s2 = set(ar2)
+s3 = set(ar3)
+
+final_set = s1.intersection(s2,s3)
+final_list = list(final_set)
+print(type(final_list))
+print(final_list)
+
 
---
+++ python collections(arrays)/set/min_max.py
@@ -0,0 +1,7 @@
+num = {34,67,45,12,99,110,543}
+
+min_value = min(num)
+max_value = max(num)
+
+print(min_value)
+print(max_value)
