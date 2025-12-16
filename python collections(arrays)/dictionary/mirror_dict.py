40c033a1286d55983213da2dd9045ebc25a66840
Gounderpriyanka <gounderpriyanka4@gmail.com>
2025-12-16 23:36:42 +0530

    updated project files and added new python scripts

--- python collections(arrays)/dictionary/mirror_dict.py
+++ python collections(arrays)/dictionary/mirror_dict.py
@@ -1,8 +1,16 @@
-n =(input("Enter the word:"))
+n = list(input("Enter the word:"))
 print (n)
 num = int(input("Enter the N-th position: "))
-s1 = n[0 : n-1]
-s2 = n[n-2 : ]
+
+s1 = n[0 : num-1]
+s2 = n[num-2 : ]
 
 print(s1)
 print(s2)
+
+a1 = "abcdefghijklmnopqrstuvwxyz"
+a2 = a1[::-1] # i didnot mention the starting point and ending
+#point so it will concat whole variable and using the -1 it take value from right to left
+a3 = dict(zip(a1,a2))
+print(a3)
+
